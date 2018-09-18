cf91d97d86e9eb648ac1906561305bea5714c670
jacob <y.zhikharev@greenlabs.ru>
2018-09-18 17:05:37 +0300

    commit

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
     url(r'^$', views.articles_list, name="list"),
-    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail")
+    url(r'^create', views.article_create, name="create"),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
+    
 ]
--- djangonautic/djangonautic/settings.py
+++ djangonautic/djangonautic/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'articles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,6 @@ STATIC_URL = '/assets/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
